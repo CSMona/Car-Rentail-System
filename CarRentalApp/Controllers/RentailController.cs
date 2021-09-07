6b3d9bd0c22e90a96acd9b6ac6fc5cda2330c305
CSMona <monasallam400@gmail.com>
2021-09-07 18:30:12 +0200

    first commit car rentail system

---
+++ CarRentalApp/Controllers/RentailController.cs
@@ -0,0 +1,78 @@
+﻿using CarRentalApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarRentalApp.Controllers
+{
+    public class RentailController : Controller
+    {
+        SuperCarEntities db = new SuperCarEntities();
+        // GET: Rentail
+        public ActionResult Index()
+        {
+            var result = (from r in db.rentails
+                          join c in db.CarRegs on r.carid equals c.carno
+                          select new RentailViewModel
+                          {
+                              id=r.id,
+                              carid=r.carid,
+                              custid=r.custid,
+                              fee=r.fee,
+                              sdate=r.sdate,
+                              edate=r.edate,
+                              available=c.available 
+
+                          }).ToList();
+            return View(result);
+        }
+        [HttpGet]
+        public ActionResult Getcar()
+        {
+            var car = db.CarRegs.ToList();
+            return Json(car, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Getid(int id)
+        {
+            var customer = (from s in db.Customers where s.id == id select s.custname).ToList();
+            return Json(customer, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Getavil(string carno)
+        {
+            var caravil = (from s in db.CarRegs where s.carno == carno select s.available).FirstOrDefault();
+          
+            return Json(caravil, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Save(rentail rent)
+        {
+            if(ModelState.IsValid)
+            {
+                db.rentails.Add(rent);
+                var car = db.CarRegs.SingleOrDefault(e => e.carno == rent.carid);
+                if (car == null)
+                {
+                    return HttpNotFound("CarNo not valid");
+                }
+                car.available = "no";
+                db.Entry(car).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rent);
+        }
+
+
+
+
+
+    }
+}
---
+++ CarRentalApp/Controllers/ReturnCarController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarRentalApp.Models;
+using System.Data.Entity.SqlServer;
+using System.Data.Entity;
+
+namespace CarRentalApp.Controllers
+{
+    public class ReturnCarController : Controller
+    {
+
+        SuperCarEntities db = new SuperCarEntities();
+        // GET: ReturnCar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Save(returncar recar) 
+        {
+            if (ModelState.IsValid)
+            {
+                db.returncars.Add(recar);
+                var car = db.CarRegs.SingleOrDefault(e => e.carno == recar.carno);
+                if (car == null)
+                {
+                    return HttpNotFound("CarNo not found");
+                }
+
+                car.available = "yes";
+                db.Entry(car).State =EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(recar);
+        }
+
+
+
+
+
+
+
+        [HttpPost]
+        public ActionResult Getid(string carno)
+        {
+            var carn = (from s in db.rentails where s.carid == carno
+                        select new
+                        {
+                            StartDate=s.sdate,
+                            EndDate=s.edate,
+                            Custid=s.custid,
+                            CarNo=s.carid,
+                            Fee=s.fee,
+                            ElapseDays = SqlFunctions.DateDiff("day",s.edate,DateTime.Now)
+
+                        }).ToArray();
+
+
+
+            return Json(carn,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
